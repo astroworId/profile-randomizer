3fc4637100e260af9e7c74ed4601eb01b242773d
cactus jack <41268557+astroworId@users.noreply.github.com>
2018-09-25 15:47:15 -0400

    added txt file export

--- address.js
+++ address.js
@@ -3,6 +3,15 @@ const config = JSON.parse(fs.readFileSync('addressconfig.json'));
 const quantity = config.numberoftimes;
 const addressline1 = config.address.address_line_1
 
+fs.writeFileSync('addy.txt', `Success! Your address was randomized ${quantity} times!\n\n\n`)
+
+console.log('-------------------------------------------')
+console.log("-------Made by:----------------------------")
+console.log('-------Discord: @cactus jack#0001----------')
+console.log('-------Twitter: @stroworld-----------------')
+console.log('-------------------------------------------')
+console.log('')
+
 
 for (repeat = 0; repeat < quantity; repeat++) {
     var text = "";
@@ -12,11 +21,6 @@ for (repeat = 0; repeat < quantity; repeat++) {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     
     let randomizedaddress = text + addressline1
-    console.log(randomizedaddress)
-
-  fs.writeFile('addy.txt', randomizedaddress, function (err) {
-    if (err) 
-        return console.log(err);
-    console.log('check addy.txt');
-});
+    fs.appendFileSync('addy.txt', randomizedaddress)
 }
+console.log(`Success! Your address was randomized ${quantity} times!  Please see addy.txt for a list of your randomized addresses.`)
