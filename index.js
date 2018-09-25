87c86e3d6ef4b35a8f96fecd6faf06750272f026
cactus jack <41268557+astroworId@users.noreply.github.com>
2018-09-25 17:26:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('config.json'));
 const quantity = config.numberoftimes;
 const addressline1 = config.address.address_line_1
+const prompt = require('prompt');
+prompt.get(['addressline'], function (err, result) {
+console.log('Address Line 1: ' + `result.addressline \n`);
+
+prompt.start();
+console.log(`Success! Your address was randomized ${quantity} times!  Please see addy.txt for a list of your randomized addresses.`)
 
 fs.writeFileSync('addy.txt', `Success! Your address was randomized ${quantity} times!\n\n\n`)
-
-console.log('-------------------------------------------')
-console.log("-------Made by:----------------------------")
-console.log('-------Discord: @cactus jack#0001----------')
-console.log('-------Twitter: @stroworld-----------------')
-console.log('-------------------------------------------')
-console.log('')
-
 
 for (repeat = 0; repeat < quantity; repeat++) {
     var text = "";
@@ -20,7 +18,7 @@ for (repeat = 0; repeat < quantity; repeat++) {
     for (var i = 0; i < 4; i++)
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     
-    let randomizedaddress = text + addressline1
+    let randomizedaddress = text + str(result.addressline)
     fs.appendFileSync('addy.txt', randomizedaddress)
 }
-console.log(`Success! Your address was randomized ${quantity} times!  Please see addy.txt for a list of your randomized addresses.`)
+});
